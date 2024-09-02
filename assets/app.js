956dc9a716a515b8a082235f7e27b472bb06f588
jadit19 <jadit19@gmail.com>
2024-09-02 06:31:27 +0530

    update UI

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', async function () {
+const about = async () => {
   const response = await fetch('/about')
   response.headers.forEach((value, name) => {
     if (name === 'expresso') {
-      document.querySelector('i').innerHTML = value
+      document.getElementById('version').innerHTML = value
     }
   })
 
@@ -24,4 +24,35 @@ document.addEventListener('DOMContentLoaded', async function () {
   const creator = document.getElementById('creator')
   creator.innerText = data?.creator?.name
   creator.href = data?.creator?.github
-})
+  creator.target = '_blank'
+
+  document.querySelectorAll('.github').forEach(element => {
+    element.href = data?.repository
+    element.target = '_blank'
+  })
+}
+
+const contributors = async () => {
+  const response = await fetch(
+    'https://api.github.com/repos/coding-cpp/expresso/contributors'
+  )
+  const data = await response.json()
+
+  const contributorsList = document.querySelector('ul')
+  data.forEach(contributor => {
+    // for (let i = 0; i < 100; i++) {
+    const listItem = document.createElement('li')
+    const a = document.createElement('a')
+    const img = document.createElement('img')
+    a.href = contributor.html_url
+    a.target = '_blank'
+    img.src = contributor.avatar_url
+    a.appendChild(img)
+    listItem.appendChild(a)
+    contributorsList.appendChild(listItem)
+    // }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', about)
+document.addEventListener('DOMContentLoaded', contributors)
